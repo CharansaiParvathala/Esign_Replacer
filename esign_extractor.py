64a04d048f864276701bb0cbf54c593248936b50
CharansaiParvathala <154997286+CharansaiParvathala@users.noreply.github.com>
2025-02-03 16:45:06 +0530

    Update esign_extractor.py

--- esign_extractor.py
+++ esign_extractor.py
@@ -1,12 +1,12 @@
-from rembg import remove
 import streamlit as st
 from io import BytesIO
 from PIL import Image
+import backgroundremover
 
 def get_esign():
     rawsign = st.file_uploader('Signature Image', type=["jpg", "jpeg", "png"])
     if rawsign:
         img_bytes = rawsign.read()
-        esign_bytes = remove(img_bytes)
+        esign_bytes = backgroundremover.remove_background(img_bytes)
         esign = Image.open(BytesIO(esign_bytes))
         return esign
